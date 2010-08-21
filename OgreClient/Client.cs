7dbcc81106b0333ab0637efc37a0f8d4caeb2b17
Benjamin Manns <benmanns@gmail.com>
2010-08-20 23:52:05 -0400

    Created Client class.

---
+++ OgreClient/Client.cs
@@ -0,0 +1,12 @@
+﻿using OgreIsland.Sockets;
+
+namespace OgreIsland.Client
+{
+    public class Client : Socket
+    {
+        public Client()
+            : base(Protocol.Delimited, Protocol.Xml)
+        {
+        }
+    }
+}
